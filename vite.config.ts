2ef79a4fed790efc154b462d6f72c39b8abb8976
Serhii Sovhut <sergey.sovgut@outlook.com>
2026-01-22 00:01:19 +0200

    removed zustand from externals

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
       formats: ["es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime", "zustand"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
     },
   },
 });
